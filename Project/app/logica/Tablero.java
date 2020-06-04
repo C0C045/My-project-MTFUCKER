81e7e89c026e951e4934d7cda233e71707f14855
repl.it user <replituser@example.com>
2020-06-04 22:12:43 +0000

    Quivo

---
+++ Project/app/logica/Tablero.java
@@ -0,0 +1,23 @@
+package app.logica
+
+public class Tablero{
+  private Ficha tableroJuego[][] = new Ficha [3][3];
+
+  public void llenar (){
+    for (int j=0; j<3; j++){
+      for(int i=0; i<3; i++){
+        tableroJuego[j][i] = new Ficha ();
+        tableroJuego[j][i].setForma("-");
+      }
+    }
+  }
+
+  public void ver(){
+    for (int j=0; j<3; j++){
+      for(int i=0; i<3; i++){
+        System.out.print(tableroJuego[j][i].getForma() + " ");
+      }
+      System.out.println("");
+    }
+  }
+}
